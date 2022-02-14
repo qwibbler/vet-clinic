105df987af9bdfb50cb8f682364027ccc85b73b0
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-02-14 12:59:01 -0600

    Add schema, data queries

--- data.sql
+++ data.sql
@@ -1,5 +1,33 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+values ('Agumon', '2020-02-03', 10.23, true, 0);
+insert into animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+values ('Gabumon', '2018-11-15', 8, true, 2);
+insert into animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+values ('Pikachu', '2021-01-07', 15.04, false, 1);
+insert into animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+values ('Devimon', '2017-05-12', 11, true, 5);
