6c2fc384a7331fe7a64a55f36b6be87b2a2147a5
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-02-15 17:54:02 -0600

    Day 2

--- data.sql
+++ data.sql
@@ -1,33 +1,123 @@
 /* Populate database with sample data. */
-insert into animals (
+INSERT INTO animals (
     name,
     date_of_birth,
     weight_kg,
     neutered,
     escape_attempts
   )
-values ('Agumon', '2020-02-03', 10.23, true, 0);
-insert into animals (
+VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
+INSERT INTO animals (
     name,
     date_of_birth,
     weight_kg,
     neutered,
     escape_attempts
   )
-values ('Gabumon', '2018-11-15', 8, true, 2);
-insert into animals (
+VALUES ('Gabumon', '2018-11-15', 8, true, 2);
+INSERT INTO animals (
     name,
     date_of_birth,
     weight_kg,
     neutered,
     escape_attempts
   )
-values ('Pikachu', '2021-01-07', 15.04, false, 1);
-insert into animals (
+VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
+INSERT INTO animals (
     name,
     date_of_birth,
     weight_kg,
     neutered,
     escape_attempts
   )
-values ('Devimon', '2017-05-12', 11, true, 5);
+VALUES ('Devimon', '2017-05-12', 11, true, 5);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+
+VALUES ('Charmander', '2020-02-08', -11, FALSE, 0);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES ('Plantmon', '2022-11-15', -5.7, TRUE, 2);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES ('Squirtle', '1993-04-02', -12.13, FALSE, 3);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES ('Angemon', '2005-06-12', -45, TEUE, 1);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES ('Boarmon', '2006-01-7', -20.4, TRUE, 7);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES ('Blossom', '1998-10-13', -20.4, TRUE, 7);
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+SELECT * FROM animals;
+UPDATE animals
+SET species = 'pokemon'
+WHERE NOT name LIKE '%mon';
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals;
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+SELECT * FROM animals;
+ROLLBACK TO SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-create table animals (
-    id int GENERATED ALWAYS AS IDENTITY,
-    name varchar(50),
-    date_of_birth date,
-    escape_attempts int,
-    neutered boolean,
-    weight_kg decimal,
-    primary key (id)
+CREATE TABLE table animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE animals ADD species VARCHAR(50);
